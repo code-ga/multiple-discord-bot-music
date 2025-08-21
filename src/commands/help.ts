c289ce1047ee0df2ad1b0cd149f6b5d21fa156f1
Code-ga <tritranduc11@gmail.com>
2025-08-21 13:43:34 +0700

    Add message command support to help and invites commands

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,6 +1,6 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Command } from '@sapphire/framework';
-import { EmbedBuilder } from 'discord.js';
+import { Args, Command } from '@sapphire/framework';
+import { EmbedBuilder, Message } from 'discord.js';
 
 @ApplyOptions<Command.Options>({
   description: 'Hiển thị hướng dẫn sử dụng bot',
@@ -9,6 +9,26 @@ import { EmbedBuilder } from 'discord.js';
 export class HelpCommand extends Command {
   public override registerApplicationCommands(registry: Command.Registry) {
     process.env.INDEX == "0" && registry.registerChatInputCommand((builder) => builder.setName('help').setDescription('Hiển thị hướng dẫn sử dụng bot'));
+  }
+
+  public override async messageRun(message: Message, _args: Args) {
+    const commands = [...this.container.client.stores.get("commands").values()];
+
+    const embed = new EmbedBuilder()
+      .setTitle('✨ Command List ✨')
+      .setColor([248, 189, 208]) // Using RGB for a softer pink
+      .setDescription(`Welcome to the command list! of **${this.container.client.user?.username}**!`)
+      .addFields(
+        commands.map(command => ({
+          name: `${this.container.client.config.prefix}${command.name}`,
+          value: `> ${command.description}`, // Adding a subtle indent
+          inline: false
+        }))
+      )
+      .setFooter({ text: 'Use ' + `${this.container.client.config.prefix}<command>` + ' to execute commands' });
+
+    await message.reply({ embeds: [embed] });
+
   }
 
   public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
--- src/commands/invites.ts
+++ src/commands/invites.ts
@@ -1,6 +1,6 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Command } from '@sapphire/framework';
-import { EmbedBuilder } from 'discord.js';
+import { Args, Command } from '@sapphire/framework';
+import { EmbedBuilder, Message } from 'discord.js';
 
 @ApplyOptions<Command.Options>({
   description: 'Shows the bot\'s invite link',
@@ -10,10 +10,19 @@ export class InviteCommand extends Command {
     process.env.INDEX == "0" && registry.registerChatInputCommand((builder) => builder.setName(this.name).setDescription(this.description));
   }
 
+  public override async messageRun(message: Message, _args: Args) {
+    const invites = await this.container.client.handlePlay.getInviteLink();
+    const embed = new EmbedBuilder()
+      .setTitle('Invite')
+      .setColor([248, 189, 208]) // Using RGB for a softer pink
+      .setDescription(`To listen music in many voice with one prefix you can invite bot (as many as you need) to your server. \n\nInvite Link: \n ${invites.invites.map(invite => `[${invite.username}](${invite.inviteLink}) <@${invite.userID}>`).join('\n')}`);
+
+    await message.reply({ embeds: [embed] });
+  }
+
   public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
 
     const invites = await this.container.client.handlePlay.getInviteLink();
-    console.log(invites)
     const embed = new EmbedBuilder()
       .setTitle('Invite')
       .setColor([248, 189, 208]) // Using RGB for a softer pink
