2880cb8bb836f416ea14e6fb472830dbad354c03
Code-ga <tritranduc11@gmail.com>
2025-08-21 23:10:11 +0700

    chore(config): Remove default lavalink nodes

--- src/config.ts
+++ src/config.ts
@@ -10,15 +10,4 @@ export const config = {
 export const LavalinkNodes: NodeOption[] = [
 	...fs.existsSync('./lavalink.json') ? JSON.parse(fs.readFileSync('./lavalink.json', 'utf-8')) :
 		[],
-	{
-		url: "lava-v4.ajieblogs.eu.org:443",
-		name: "Ajieblogs",
-		auth: "https://dsc.gg/ajidevserver",
-		secure: true,
-	},
-	{
-		url: "lavalinkv4.serenetia.com:443",
-		name: "Serenetia",
-		auth: "https://dsc.gg/ajidevserver",
-	}
 ];
